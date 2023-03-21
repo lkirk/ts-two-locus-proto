5550b0b051fd59130ef48ce20f625b6977e6a888
lloydkirk <kirklloyd@gmail.com>
2023-03-21 15:05:25 -0500

    Implement two different norming strategies
    Implements the total and hap weighted norming strategies. I've included
    a lot of debugging code to help track down erroneous results.
    
    Pylint is not happy about these changes, but I'm ignoring it for now.

--- tskit_ld_proto/ld_proto.py
+++ tskit_ld_proto/ld_proto.py
@@ -1,4 +1,5 @@
 import io
+from enum import Enum
 
 import numpy as np
 import tskit
@@ -105,13 +106,11 @@ def pairs_with_replacement_idx(n):
         subloop_start += 1
 
 
-def get_state(ts):
+def get_state(ts, state, num_states):
     """
     :param ts: tree sequence object
 
     """
-    state = np.zeros((ts.num_sites, ts.num_samples), dtype=int)
-    num_states = np.zeros(ts.num_sites, dtype=int)
     for tree in ts.trees():
         for site in tree.sites():
             current_state = 0
@@ -120,52 +119,20 @@ def get_state(ts):
                 current_state += 1
                 for sample_id in tree.samples(mutation.node):
                     state[site.id, sample_id] = current_state
-            num_states[site.id] = current_state
-    return num_states, state
+            num_states[site.id] = current_state + 1
 
 
-def get_allele_weights(state, num_states):
+def get_allele_weights(a_state, b_state, num_samples, haplotype_states):
     """
-    >>> list(get_allele_weights(# doctest: +NORMALIZE_WHITESPACE
-    ...     np.array([[0, 0, 1, 0],
-    ...               [1, 0, 0, 0],
-    ...               [0, 1, 1, 1]]),
-    ...     np.array([1, 1, 1])))
-    [(0, 0, (3, 0, 0)),
-     (0, 1, (2, 1, 1)),
-     (0, 2, (1, 2, 0)),
-     (1, 1, (3, 0, 0)),
-     (1, 2, (0, 3, 1)),
-     (2, 2, (1, 0, 0))]
+    >>> haplotype_states = np.zeros((2, 3))
+    >>> get_allele_weights([0, 0, 1, 0, 0], [1, 2, 0, 0, 1], 5, haplotype_states)
+    >>> haplotype_states
+    array([[1., 2., 1.],
+           [1., 0., 0.]])
 
     """
-    for A_site_idx, B_site_idx in pairs_with_replacement_idx(len(state)):
-        # print(f'{A_site_idx},{B_site_idx},{A_samples},{B_sample_state}')
-        A_sample_state = state[A_site_idx]
-        B_sample_state = state[B_site_idx]
-        A_dim = num_states[A_site_idx]
-        B_dim = num_states[B_site_idx]
-
-        haplotype_counts = np.zeros((A_dim + 1, B_dim + 1), dtype=int)
-        for A_state, B_state in zip(A_sample_state, B_sample_state):
-            haplotype_counts[A_state, B_state] += 1
-
-        # print(haplotype_counts)
-        for A in range(A_dim):
-            for B in range(B_dim):
-                w_AB = haplotype_counts[A, B]
-                w_Ab = sum(haplotype_counts[A, idx] for idx in leave_out_wraparound(B_dim + 1, B))
-                w_aB = sum(haplotype_counts[idx, B] for idx in leave_out_wraparound(A_dim + 1, A))
-                yield A_site_idx, B_site_idx, (w_AB, w_Ab, w_aB)
-
-
-# TODO: why wrong?
-# def compute_D(w_AB, w_Ab, w_aB, n):
-#     p_AB = w_AB / float(n)
-#     p_Ab = w_Ab / float(n)
-#     p_aB = w_aB / float(n)
-#     p_ab = 1 - p_AB
-#     return p_AB * p_ab - p_Ab * p_aB
+    for i in range(num_samples):
+        haplotype_states[a_state[i], b_state[i]] += 1
 
 
 def compute_D(w_AB, w_Ab, w_aB, n):
@@ -212,14 +179,78 @@ def compute_r2(w_AB, w_Ab, w_aB, n):
     return (D * D) / denom
 
 
-def compute_stat_matrix(ts, summary_func):
-    num_states, state = get_state(ts)
-    matrix = np.zeros((len(state), len(state)))
-    for i, j, w in get_allele_weights(state, num_states):
-        matrix[i, j] = summary_func(*w, state.shape[1])
+def two_site_general_stat(ts, summary_func, norm_method):
+    """
+    Wrapper for python prototyping, inline in final version
+    """
+    state = np.zeros((ts.num_sites, ts.num_samples), dtype=int)
+    num_states = np.zeros(ts.num_sites, dtype=int)
+    get_state(ts, state, num_states)
+    return _two_site_general_stat(state, num_states, summary_func, norm_method)
 
-    # reflect upper triangle to lower triangle
-    lower_triangle_idx = np.tril_indices(len(matrix), k=-1)
-    matrix[lower_triangle_idx] = matrix.T[lower_triangle_idx]
 
-    return matrix
+class NormMethod(Enum):
+    """
+    Method for norming the output statistic
+
+    TOTAL: Divide the statistic by the total number of haplotypes
+    HAP_WEIGHTED: Weight each allele's statistic by the proportion of the haplotype present
+    """
+
+    TOTAL = "total"
+    HAP_WEIGHTED = "hap_weighted"
+
+
+def _two_site_general_stat(state, num_states, summary_func, norm_method, polarized=False, debug=False):
+    norm = NormMethod(norm_method)
+    result = np.zeros((len(state), len(state)))
+    for a_site_id, b_site_id in pairs_with_replacement_idx(len(state)):
+        # TODO: compute_general_two_site_stat_result
+        a_dim = num_states[a_site_id]
+        b_dim = num_states[b_site_id]
+
+        haplotype_states = np.zeros((a_dim, b_dim), dtype=int)
+        get_allele_weights(state[a_site_id], state[b_site_id], state.shape[1], haplotype_states)
+
+        stat_result = 0  # TODO: what is a sensible default here?
+        if debug:
+            print(f"a: {state[a_site_id]} b: {state[b_site_id]}")
+            print(haplotype_states)
+            if norm is NormMethod.HAP_WEIGHTED:
+                print("wAB\twAb\twaB\tstat\thap_prop")
+            else:
+                print("wAB\twAb\twaB")
+        for a_idx in range(1 if polarized else 0, a_dim):
+            for b_idx in range(1 if polarized else 0, b_dim):
+                w_AB = haplotype_states[a_idx, b_idx]
+                w_Ab = sum(haplotype_states[a_idx, idx] for idx in leave_out_wraparound(b_dim, b_idx))
+                w_aB = sum(haplotype_states[idx, b_idx] for idx in leave_out_wraparound(a_dim, a_idx))
+
+                if norm is NormMethod.TOTAL:
+                    stat_result += summary_func(w_AB, w_Ab, w_aB, state.shape[1])
+                    if debug:
+                        print(w_AB, w_Ab, w_aB, summary_func(w_AB, w_Ab, w_aB, state.shape[1]))
+                elif norm is NormMethod.HAP_WEIGHTED:
+                    haplotype_prop = haplotype_states[a_idx, b_idx] / haplotype_states.sum().sum()
+
+                    stat_result += haplotype_prop * summary_func(w_AB, w_Ab, w_aB, state.shape[1])
+                    if debug:
+                        print(
+                            w_AB, w_Ab, w_aB, summary_func(w_AB, w_Ab, w_aB, state.shape[1]), haplotype_prop, sep="\t"
+                        )
+                else:
+                    raise ValueError(f"unhandled norm method type: {norm}")
+        if debug:
+            print("--------------------------------------------------------")
+
+        if norm is NormMethod.TOTAL:
+            stat_result /= (a_dim - (1 if polarized else 0)) * (b_dim - (1 if polarized else 0))
+        result[a_site_id, b_site_id] = stat_result
+
+        # TODO: how to do this in C?
+        # TODO: actually we won't, this will be handled by downstream python machinery, so this method is fine.
+        # reflect upper triangle to lower triangle
+        lower_triangle_idx = np.tril_indices(len(result), k=-1)
+        result[lower_triangle_idx] = result.T[lower_triangle_idx]
+
+    return result
